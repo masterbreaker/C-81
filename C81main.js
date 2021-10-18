0f54ef0e91608d6022877aa3fc07cc3f3d776fb1
Moradabad <75120768+masterbreaker@users.noreply.github.com>
2021-10-18 13:52:06 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,36 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+color="black";
+ctx.beginPath();
+
+ctx.strokeStyle=color;
+ctx.lineWidth=3;
+
+ctx.arc(200,200,75,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e)
+{
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+
+    console.log("x= "+mouse_x+", y= "+ mouse_y);
+
+    circle(mouse_x,mouse_y);
+
+}
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+
+    ctx.strokeStyle=color;
+    ctx.lineWidth=3;
+    
+    ctx.arc(mouse_x,mouse_y,75,0,2*Math.PI);
+    ctx.stroke();
+       
+}
+function clearArea(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
